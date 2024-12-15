7490c06a79db9f7cacfa1203be4c14a4109494cc
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-12-15 10:40:21 -0300

    Adicionando dependência javafaker

--- proto-api/src/main/java/com/cqrs/protoAPI/ProtoApiApplication.java
+++ proto-api/src/main/java/com/cqrs/protoAPI/ProtoApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProtoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProtoApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoApiApplication.class, args);
+    }
 
 }
--- proto-api/src/test/java/com/cqrs/protoAPI/ProtoApiApplicationTests.java
+++ proto-api/src/test/java/com/cqrs/protoAPI/ProtoApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProtoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
