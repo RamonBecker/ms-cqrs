4c5835769c4e3a6ca20fd0cd6b4d10227aa853fa
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-12-15 10:28:17 -0300

    Adicionando arquivos iniciais

---
+++ protoAPI/src/test/java/com/cqrs/protoAPI/ProtoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqrs.protoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
