9c320d89c3869818dbb864ba6d78c61efb2917b5
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-12-22 09:04:31 -0300

    Criação de relacionamentos entre tabelas

--- ms-beautique/beautique/src/main/java/br/com/beautique/entities/CustomerEntity.java
+++ ms-beautique/beautique/src/main/java/br/com/beautique/entities/CustomerEntity.java
@@ -4,29 +4,25 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "beauty_procedures")
-public class BeautyProceduresEntity extends BaseEntity {
-
+@Entity
+@Table(name = "customer")
+public class CustomerEntity extends BaseEntity {
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(length = 500)
-    private String description;
-
-    @Column(nullable = false)
-    private BigDecimal price;
+    @Column(nullable = false, length = 100)
+    private String phone;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "beautyProcedures", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private List<AppointmentsEntity> appointments;
+
 }
